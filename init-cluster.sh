b49ebabb0ad5a51cb5a74e679db16afab5e018c0
Simba Chiu <schiu@hetcak.nl>
2025-03-10 18:06:02 +0100

    WIP

--- init-cluster.sh
+++ init-cluster.sh
@@ -1,4 +1,4 @@
 kubectl create namespace argocd
 kubectl create namespace dev
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.14.4/manifests/ha/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f base/applicationset.yaml
